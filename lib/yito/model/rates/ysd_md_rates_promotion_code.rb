1c54584fc828589096a3c78c8a984d708ca8877c
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2016-02-16 01:46:05 +0100

    discount + promotion code

--- lib/yito/model/rates/ysd_md_rates_promotion_code.rb
+++ lib/yito/model/rates/ysd_md_rates_promotion_code.rb
@@ -17,6 +17,13 @@ module Yito
         property :date_to, DateTime
         property :discount_type, Enum[:percentage, :amount], :default => :percentage      
         property :value, Decimal, :scale => 2, :precision => 10, :default => 0
+
+        # Check if there are active discounts
+        #
+        def self.active?(date)
+          count(:date_from.lte => date, :date_to.gte => date )
+        end
+
       end
     end
   end

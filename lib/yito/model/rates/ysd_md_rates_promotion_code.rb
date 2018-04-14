0fabc3a03dea17af9579a941f6df57648693beb9
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2018-04-15 01:43:02 +0200

    check valid promotion code

--- lib/yito/model/rates/ysd_md_rates_promotion_code.rb
+++ lib/yito/model/rates/ysd_md_rates_promotion_code.rb
@@ -18,10 +18,37 @@ module Yito
         property :discount_type, Enum[:percentage, :amount], :default => :percentage      
         property :value, Decimal, :scale => 2, :precision => 10, :default => 0
 
+        # -------------------------- Class methods ---------------------------------------------------------
+
         # Check if there are active discounts
         #
         def self.active?(date)
           count(:date_from.lte => date, :date_to.gte => date )
+        end
+
+        #
+        # Check if the promotion code is valid
+        #
+        def self.valid_code?(promotion_code)
+          if promotion_code = ::Yito::Model::Rates::PromotionCode.first(promotion_code: promotion_code)
+            promotion_code.valid_code?
+          else
+            return false
+          end
+        end
+
+        # ------------------------- Instance methods ------------------------------------------------------
+
+        #
+        # Check if the promotion code is valid
+        #
+        def valid_code?
+          today = Date.today
+          if today >= self.date_from && today <= self.date_to
+            return true
+          else
+            return false
+          end
         end
 
       end
